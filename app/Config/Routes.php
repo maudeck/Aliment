d19933a2fe81a9a3c4e07bf8008adf38b1c59370
Giovan2005 <mamihajagiovan2005@gmail.com>
2026-05-09 16:38:51 +0300

    gestion portefeuille
    Add controlleur Portefeuille
    Add Route
    Add Models Portefeuille
    Add page portefeuille
    +donnes de teste

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,10 +15,10 @@ $routes->get('/home', 'Home::index');
 // ============================================
 // Routes d'INSCRIPTION (2 étapes)
 // ============================================
-$routes->get('/register', 'Register::index');                    // Étape 1 - Formulaire
-$routes->post('/register/store', 'Register::store');            // Étape 1 - Traitement
-$routes->get('/register/step2', 'Register::step2');             // Étape 2 - Formulaire
-$routes->post('/register/step2/store', 'Register::store2');     // Étape 2 - Traitement
+$routes->get('/register', 'Register::index');
+$routes->post('/register/store', 'Register::store');
+$routes->get('/register/step2', 'Register::step2');
+$routes->post('/register/step2/store', 'Register::store2');
 
 // ============================================
 // Routes de LOGIN
@@ -27,11 +27,17 @@ $routes->get('/login', 'Login::index');
 $routes->post('/login/authenticate', 'Login::authenticate');
 $routes->get('/logout', 'Login::logout');
 
-
 $routes->get('/register/objectif', 'Register::objectif');
 $routes->post('/register/objectif/store', 'Register::storeObjectif');
 
 $routes->get('/home',          'Home::index');
 $routes->post('/home/acheter', 'Home::acheter');
 $routes->get('/regimes',       'Regimes::index');
- 
+
+// ============================================
+// Routes PORTEFEUILLE
+// ============================================
+$routes->get('/portefeuille',            'PortefeuilleController::index');
+$routes->post('/portefeuille/recharger', 'PortefeuilleController::recharger');
+
+$routes->post('home/recharger', 'Home::recharger');
--- app/Views/pages/regimes.php
+++ app/Views/pages/regimes.php
@@ -27,7 +27,7 @@
         <a class="nav-link" href="<?= base_url('/register/objectif'); ?>">Objectifs</a>
         <a class="nav-link active" href="<?= base_url('/regimes'); ?>">Régimes</a>
         <a class="nav-link" href="#">Activités</a>
-        <a class="nav-link" href="#">Portefeuille</a>
+        <a class="nav-link" href="<?= base_url('/portefeuille'); ?>">Portefeuille</a>
         <a class="nav-link" href="#">Option Gold</a>
       </nav>
     </div>
